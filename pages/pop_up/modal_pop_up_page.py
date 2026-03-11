8f49c9990c4c993021ebe67afba0adedbfa4c21d
yevge <yevgeniysarancha@gmail.com>
2026-03-11 13:33:36 +0100

    test & POM: create pop up page and add test for pop up box

--- pages/pop_up/modal_pop_up_page.py
+++ pages/pop_up/modal_pop_up_page.py
@@ -9,6 +9,13 @@ class ModalPopUpPage(BaseResultPage):
     def __init__(self, page: Page):
         super().__init__(page)
         self.launch_button = page.get_by_role("button", name="Launch Pop-Up")
+
         self.modal = page.locator("#exampleModal")
+
+        self.modal_title = self.modal.locator(".modal-header")
         self.checkbox = self.modal.locator("#id_checkbox_0")
-        self.send_button = self.modal.get_by_role("button", name="Send")
+        self.checkbox_label = self.modal.locator("label[for='id_checkbox_0']")
+
+        self.send_button = self.modal.get_by_text("Send")
+        self.cancel_button = self.modal.get_by_text("Close")
+
--- tests/pop_up/test_modale_pop_up.py
+++ tests/pop_up/test_modale_pop_up.py
@@ -6,9 +6,17 @@ def test_modal_pop_up(app):
     page.open()
 
     expect(page.launch_button).to_be_visible()
+    expect(page.launch_button).to_have_text("Launch Pop-Up")
+
     page.launch_button.click()
     expect(page.modal).to_be_visible()
-    expect(page.checkbox).to_be_visible()
 
+    expect(page.checkbox).to_be_visible()
     page.checkbox.check()
     expect(page.checkbox).to_be_checked()
+
+    expect(page.cancel_button).to_be_visible()
+    expect(page.send_button).to_be_visible()
+
+    page.send_button.click()
+    expect(page.result_text).to_have_text("select me or not")
